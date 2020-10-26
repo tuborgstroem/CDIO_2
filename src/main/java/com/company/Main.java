809110e90dad781b292b019f6d580892e533770e
Kian Lund <lundk90@gmail.com>
2020-10-26 11:34:42 +0100

    Updated main.java as mentioned in previous commit

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -2,20 +2,9 @@ package main.java.com.company;
 
 import main.java.game.Game;
 
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-        Scanner inp = new Scanner(System.in);
-        String opt;
-        while (true) {
-            System.out.println("Please enter 'Play' to play dice roll game");
-            opt = inp.nextLine();
-            if (opt.equalsIgnoreCase("play")) {
-                Game a = new Game(2, 2);
-                break;
-            }
-        }
+        new Game();
     }
 }
