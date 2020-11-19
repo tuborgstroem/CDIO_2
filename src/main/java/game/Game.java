43bcd8ed61a2154af45032dfe91bcd462e66e892
Thor <tgregersen998@gmail.com>
2020-11-19 14:15:21 +0100

    translated to englando

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -139,7 +139,6 @@ public class Game {
     }
 
 
-
     public Player[] getPlayerList() { return playerList; }
 
     public GUI getGui() {
--- src/main/java/game/Prison.java
+++ src/main/java/game/Prison.java
@@ -5,34 +5,34 @@ import gui_main.GUI;
 import java.awt.*;
 import gui_fields.GUI_Field;
 
-    public class Prison extends SpecialFields {
+public class Prison extends SpecialFields {
 
-        public Prison(int fields, GUI gui){
-            super(fields, gui);
-        }
-        /**
-         * toString til udskrivning af besked, når spilleren lander på feltet.
-         */
-        @Override
-        public String toString(){
-            return "Du er blevet taget for at stjæle slik, og skal i fængsel";
-        }
-
-        public void setIPrison(Player player){
-            player.setPlacering(6);
-        }
-
-        /**
-         * landOnFiels indeholder fængselfeltets spil logik
-         * @param
-         */
-        @Override
-        public void landOnField(Player player) {
-            gui.showMessage(toString());
-            setIPrison(player);
-            player.setPrison(true);
-        }
-
-
+    public Prison(int fields, GUI gui){
+        super(fields, gui);
     }
+    /**
+     * toString til udskrivning af besked, når spilleren lander på feltet.
+     */
+    @Override
+    public String toString(){
+        return "You've been caught stealing candy! Go to jail";
+    }
+
+    public void setInPrison(Player player){
+        player.setLocation(6);
+    }
+
+    /**
+     * landOnFiels indeholder fængselfeltets spil logik
+     * @param
+     */
+    @Override
+    public void landOnField(Player player) {
+        gui.showMessage(toString());
+        setInPrison(player);
+        player.setInPrison(true);
+    }
+
+
+}
 
