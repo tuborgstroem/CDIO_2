863f14a23d0c228452b7923fd7bb17ab5bca0d6a
Thor <tgregersen998@gmail.com>
2020-11-19 12:52:22 +0100

    Text implementing

--- src/main/java/game/Text.java
+++ src/main/java/game/Text.java
@@ -2,13 +2,14 @@ package game;
 import gui_fields.*;
 import gui_main.GUI;
 
-public class Text (Player player){
+public class Text {
     private GUI_Field[] fields;
     private Game game;
     public final Language textStrings;
     private String tileString;
     private GameBoard board;
     private GUI gui;
+    private Player player;
     int location;
 
     public Text(Game game) {
@@ -17,106 +18,106 @@ public class Text (Player player){
         this.game = game;
         location = player.getLocation();
     }
-
-    switch (player.getLocation()) {
-                    case 1:
-                        String message = tileString.getLine(1);
-                        displayMessage(message);
-                        break;
-                    case 2:
-                         message = textStrings.getLine(2);
-                        displayMessage(message);
-                        break;
-                    case 3:
-                        message = tileString.getLine(3);
-                        displayMessage(message);
-                        break;
-                    case 4:
-                        message = tileString.getLine(4);
-                        displayMessage(message);
-                        break;
-                    case 5:
-                        message = tileString.getLine(5);
-                        displayMessage(message);
-                        break;
-                    case 6:
-                        message = tileString.getLine(6);
-                        displayMessage(message);
-                        break;
-                    case 7:
-                        message = tileString.getLine(7);
-                        displayMessage(message);
-                        break;
-                    case 8:
-                        message = tileString.getLine(8);
-                        displayMessage(message);
-                        break;
-                    case 9:
-                        message = tileString.getLine(9);
-                        displayMessage(message);
-                        break;
-                    case 10:
-                        message = tileString.getLine(10);
-                        displayMessage(message);
-                        break;
-                    case 11:
-                        message = tileString.getLine(11);
-                        displayMessage(message);
-                        break;
-                    case 12:
-                        message = tileString.getLine(12);
-                        displayMessage(message);
-                        break;
-                    case 13:
-                        message = tileString.getLine(13);
-                        displayMessage(message);
-                        break;
-                    case 14:
-                        message = tileString.getLine(14);
-                        displayMessage(message);
-                        break;
-                    case 15:
-                        message = tileString.getLine(15);
-                        displayMessage(message);
-                        break;
-                    case 16:
-                        message = tileString.getLine(16);
-                        displayMessage(message);
-                        break;
-                    case 17:
-                        message = tileString.getLine(17);
-                        displayMessage(message);
-                        break;
-                    case 18:
-                        message = tileString.getLine(18);
-                        displayMessage(message);
-                        break;
-                    case 19:
-                        message = tileString.getLine(19);
-                        displayMessage(message);
-                        break;
-                    case 20:
-                        message = tileString.getLine(20);
-                        displayMessage(message);
-                        break;
-                    case 21:
-                        message = tileString.getLine(21);
-                        displayMessage(message);
-                        break;
-                    case 22:
-                        message = tileString.getLine(22);
-                        displayMessage(message);
-                        break;
-                    case 23:
-                        message = tileString.getLine(23);
-                        displayMessage(message);
-                        break;
-                    case 24:
-                        message = tileString.getLine(24);
-                        displayMessage(message);
-                        break;
-                }
-
+    public void TileMessage (Player player) {
+        switch (player.getLocation()) {
+            case 1:
+                String message = textStrings.getLine(1);
+                displayMessage(message);
+                break;
+            case 2:
+                message = textStrings.getLine(2);
+                displayMessage(message);
+                break;
+            case 3:
+                message = textStrings.getLine(3);
+                displayMessage(message);
+                break;
+            case 4:
+                message = textStrings.getLine(4);
+                displayMessage(message);
+                break;
+            case 5:
+                message = textStrings.getLine(5);
+                displayMessage(message);
+                break;
+            case 6:
+                message = textStrings.getLine(6);
+                displayMessage(message);
+                break;
+            case 7:
+                message = textStrings.getLine(7);
+                displayMessage(message);
+                break;
+            case 8:
+                message = textStrings.getLine(8);
+                displayMessage(message);
+                break;
+            case 9:
+                message = textStrings.getLine(9);
+                displayMessage(message);
+                break;
+            case 10:
+                message = textStrings.getLine(10);
+                displayMessage(message);
+                break;
+            case 11:
+                message = textStrings.getLine(11);
+                displayMessage(message);
+                break;
+            case 12:
+                message = textStrings.getLine(12);
+                displayMessage(message);
+                break;
+            case 13:
+                message = textStrings.getLine(13);
+                displayMessage(message);
+                break;
+            case 14:
+                message = textStrings.getLine(14);
+                displayMessage(message);
+                break;
+            case 15:
+                message = textStrings.getLine(15);
+                displayMessage(message);
+                break;
+            case 16:
+                message = textStrings.getLine(16);
+                displayMessage(message);
+                break;
+            case 17:
+                message = textStrings.getLine(17);
+                displayMessage(message);
+                break;
+            case 18:
+                message = textStrings.getLine(18);
+                displayMessage(message);
+                break;
+            case 19:
+                message = textStrings.getLine(19);
+                displayMessage(message);
+                break;
+            case 20:
+                message = textStrings.getLine(20);
+                displayMessage(message);
+                break;
+            case 21:
+                message = textStrings.getLine(21);
+                displayMessage(message);
+                break;
+            case 22:
+                message = textStrings.getLine(22);
+                displayMessage(message);
+                break;
+            case 23:
+                message = textStrings.getLine(23);
+                displayMessage(message);
+                break;
+            case 24:
+                message = textStrings.getLine(24);
+                displayMessage(message);
+                break;
+        }
+    }
     private void displayMessage(String message) {
         getGui().displayChanceCard(message);
 
@@ -127,9 +128,7 @@ public class Text (Player player){
             return board;
         }
         
-        public Player player {
-        return player;
-    }
+
 
     public GUI getGui() {
         return gui;
