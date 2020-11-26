a76f246c825bec7dc0bcb4d1f900777bdb9aa641
Kian Lund <lundk90@gmail.com>
2020-11-26 12:59:23 +0100

    Removed static Language objects

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -5,8 +5,8 @@ import game.Language;
 
 public class Main {
 
-    public static final Language langStrings = new Language("resources/engGameStrings.txt");
-    public static final Language tileStrings = new Language("resources/engTileStrings.txt");
+    //public static final Language langStrings = new Language("resources/engGameStrings.txt");
+    //public static final Language tileStrings = new Language("resources/engTileStrings.txt");
 
     public static void main(String[] args) {
         boolean test = false;
